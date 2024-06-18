9d199ed8b5b6a9a195bc3aa72f0b486945de9426
AdrianPC <adrjeness@gmail.com>
2024-06-18 18:51:43 +0200

    Connected to the database

---
+++ MedicaRevolution.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MedicaRevolution.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MedicaRevolution.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("MedicaRevolutionDb");
+        services.AddDbContext<MedicaRevolutionDbContext>(options =>
+            options.UseSqlServer(connectionString)
+                .EnableSensitiveDataLogging());
+    }
+}
---
+++ MedicaRevolution.Infrastructure/Persistence/MedicaRevolutionDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicaRevolution.Infrastructure.Persistence;
+
+internal class MedicaRevolutionDbContext : DbContext
+{
+    public MedicaRevolutionDbContext(DbContextOptions<MedicaRevolutionDbContext> options) : base(options)
+    {
+    }
+}
